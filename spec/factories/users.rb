98bb344cc3cc754f955d0e436e67583e897985f0
ryan <lrcrews@gmail.com>
2015-05-26 00:02:51 -0700

    Set up the rspec and factory girl rig, created some failing tests and got them passing, we're now ready to write a bunch of tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  
+  factory :user do
+    email "rubyrodd@supergre.en"
+    password "12345678"
+    password_confirmation "12345678"
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+  end
+
+  after(:each) do
+    @user.destroy
+  end
+
+  describe "email" do
+    it "should not be allowed if another user has the same email (not unique)" do
+      user2 = FactoryGirl.build(:user)
+      expect(user2).to be_invalid
+    end
+
+    it "should be allowed if no other user has the same email (unique)" do
+      user3 = FactoryGirl.build(:user, email: "korbind@zorg.com")
+      expect(user3).to be_valid
+    end
+  end
+
+end
